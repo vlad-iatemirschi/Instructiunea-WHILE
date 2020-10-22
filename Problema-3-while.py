085638fd072887b203ed9e048d53ee6827d3c130
iatemirschi vladislav cl 10D <70971250+vlad-iatemirschi@users.noreply.github.com>
2020-10-22 19:54:42 +0300

    Add files via upload

---
+++ Problema-3-while.py
@@ -0,0 +1,8 @@
+a=0
+b=0
+while (a%2==0) or (a%2==0) and (a%3!=0):
+    a=eval(input("numarul este:"))
+    if a%2==0:
+       b=b+a
+
+print(b)
---
+++ Problema-4-while.py
@@ -0,0 +1,16 @@
+nr=0
+s=0
+p=1
+m=0
+
+n=eval(input("numarul ="))
+while (nr<n):
+    nr=nr+1
+    s=s+nr
+    p=p*nr
+    m=s/nr
+   
+print("suma =",s,)
+print("produsul =",p,)
+print("media aritmetica =",m)
+
